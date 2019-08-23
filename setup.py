a9edc0ae92939278387fb55640b81d208d851062
Joseph Mancuso <idmann509@gmail.com>
2019-08-23 17:16:49 -0400

    fixed setup versioning

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 import os
 
-with open('src/masonite/interfaces/version.txt', 'w') as file:
-    file.write(os.getenv('CIRCLE_TAG', '0.0.7').replace('v', ''))
+if not os.path.exists('src/masonite/interfaces/version.txt', 'w'):
+    with open('src/masonite/interfaces/version.txt', 'w') as file:
+        file.write(os.getenv('CIRCLE_TAG', '0.0.7').replace('v', ''))
 
 with open('src/masonite/interfaces/version.txt') as file:
     version = file.read()
